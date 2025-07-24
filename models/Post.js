b2a7e143a87cb2962e84fd5d3d11443a0b341299
Nneka <777uloma777@gmail.com>
2025-07-24 10:18:14 -0400

    first commit

---
+++ models/Post.js
@@ -0,0 +1,25 @@
+import {Schema} from 'mongoose';
+import mongoose from 'mongoose';
+
+const postSchema = new Schema({
+    "title": {
+        type: String,
+        required: true
+    },
+    "body": {
+        type: String, 
+        required: true
+    },
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+})
+
+const Post = mongoose.model("Post", postSchema);
+export default Post;
---
+++ server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import dotenv from "dotenv";
+import db from "./config/connection.js";
+import cors from 'cors';
+import userRouter from './routes/users.js';
+import postRouter from './routes/posts.js';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors({origin: "http://localhost:5173"}));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use('/api/users', userRouter);
+
+db.once("open", () => {
+  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+});
